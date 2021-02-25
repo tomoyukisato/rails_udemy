32c6f4c13453467673f6dd2dd0db34816b1583c2
tomoyukisato <funkyfunkytomo@gmail.com>
2021-02-25 14:24:29 +0900

    first init

---
+++ app/models/board.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :bigint           not null, primary key
+#  name       :string(255)
+#  title      :string(255)
+#  body       :text(65535)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Board < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 10 }
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :body, presence: true, length: { maximum: 1000 }
+end
